363fec8974594e2cc450f4389cbda3265f9da8e6
Dmitry Ilin <dm-ilin@mail.ru>
2022-01-19 13:25:58 +0400

    feature/js-start написать скрипт вызова и отправки формы

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let popupOpenButton = document.querySelector('.profile__info-edit-button'); //определили блок с кнопкой
 let popup = document.querySelector('.popup'); //Определили блок popup
-let popupCloseButton = document.querySelector('.popup__close-button');
+let popupCloseButton = document.querySelector('.popup__close-button');//определили крестик
 
 //эта фунция добавляет popup класс popup_opened
 function popupOpen() {
@@ -29,53 +29,47 @@ function popupCloseField() {
 //и вызов этой функции
 popup.addEventListener('click', popupCloseField);
 
-/*
-// Находим форму в DOM
-let formElement = // Воспользуйтесь методом querySelector()
-// Находим поля формы в DOM
-let nameInput = // Воспользуйтесь инструментом .querySelector()
-let jobInput = // Воспользуйтесь инструментом .querySelector()
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-
-    // Получите значение полей jobInput и nameInput из свойства value
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    // Вставьте новые значения с помощью textContent
-}
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler);
-*/
 
 
-let nameInput = popup.querySelector('.popup__input_set_name'); //имя
-let statusInput = popup.querySelector('.popup__input_set_status'); //статус
 
-//console.log(nameInput, statusInput);
+let nameInput = popup.querySelector('.popup__input_set_name'); //поле ввода имени
+let statusInput = popup.querySelector('.popup__input_set_status'); //поле ввода статуса
+
+//Функция отправки данных формы
 function formSubmitHandler (evt) {
   evt.preventDefault();//отменяем перезагрузку страницы
-  let name = nameInput.value; //значение inputa имя
-  let status = statusInput.value; //значение inputa статуса
-  console.log(name, status);
 
-  let pasteName = document.querySelector('.profile__info-title');//сюда вставляем имя
-  let pasteStatus = document.querySelector('.profile__info-status');// сюда вставляем статус
+  let pasteName = document.querySelector('.profile__info-title');//сюда вставляем имя (заголовок на странице)
+  let pasteStatus = document.querySelector('.profile__info-status');// сюда вставляем статус (подзаголовок на странице)
+  let name //объявили переменную, в которой хранится введенное значение имени
+  let status // объявили переменную, в которой хранится введенное значение статуса
 
-  console.log(pasteName, pasteStatus )
+  //здесь сброс значения имени, если введено пустое поле
+  if (nameInput.value !== "") {
+    name = nameInput.value; //берем значение из поля ввода
+  } else {
+    name = pasteName.textContent; //значение на странице оставляем прежним
+    nameInput.value = name; // а плейсхолдер берем уже со страницы
+  }
+
+  //здесь сброс значения статуса, если введено пустое поле
+  if (statusInput.value !== "") {
+    status = statusInput.value; //берем значение из поля ввода
+  } else {
+    status = pasteStatus.textContent; //значение на странице оставляем прежним
+    statusInput.value = status; // а плейсхолдер берем уже со страницы
+  }
+
   pasteName.textContent = name;//переносим значение инпута в заголовок
-  pasteStatus.textContent = status;
+  pasteStatus.textContent = status; //переносим значение статуса на страницу
 
   popupClose();//Закрываем popup
-
 }
-let saveButton = document.querySelector('.popup__save-button');
-saveButton.addEventListener('click', formSubmitHandler);
+
+popup.addEventListener('submit', formSubmitHandler); //выполняем функцию отправки формы
+
+
+
+
+
 
