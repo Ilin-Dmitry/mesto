b67739196ef5b3fed79001ef8e3c063b69710993
Dmitry Ilin <dm-ilin@mail.ru>
2022-01-25 22:51:20 +0400

    hotfixes в скрипте в форму добавляются значения со страницы

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,31 @@
 let popupOpenButton = document.querySelector('.profile__info-edit-button'); //определили блок с кнопкой
 let popup = document.querySelector('.popup'); //Определили блок popup
 let popupCloseButton = document.querySelector('.popup__close-button');//определили крестик
+let nameInput = popup.querySelector('.popup__input_set_name'); //поле ввода имени
+let statusInput = popup.querySelector('.popup__input_set_status'); //поле ввода статуса
+let pasteName = document.querySelector('.profile__info-title');//сюда вставляем имя (заголовок на странице)
+let pasteStatus = document.querySelector('.profile__info-status');// сюда вставляем статус (подзаголовок на странице)
 
-//эта фунция добавляет popup класс popup_opened
+
+//эта фунция добавляет popup класс popup_opened и вставляет в инпуты значения со страницы
 function popupOpen() {
   popup.classList.add('popup_opened');
+  nameInput.value = pasteName.textContent; // вставляем значение имени на страницуе в поле ввода имени
+  statusInput.value = pasteStatus.textContent; //аналогично со статусом
 }
 
-//вызываем функцию popupOpen при клике
-popupOpenButton.addEventListener('click', popupOpen);
+
+
+
 
 //функция удаляет popup_opened
 function popupClose() {
   popup.classList.remove('popup_opened');
 }
 
-//вызываем функцию удаления popup_opened, если нажали на крестик
-popupCloseButton.addEventListener('click', popupClose);
 
+
+/*
 //функцию удаления popup_opened, если нажали мимо формы
 function popupCloseField() {
   //console.log(event.target, event.currentTarget); //Если раскомментировать, увидим значения таргетов
@@ -29,42 +37,32 @@ function popupCloseField() {
 //и вызов этой функции
 popup.addEventListener('click', popupCloseField);
 
+*/
 
-
-
-let nameInput = popup.querySelector('.popup__input_set_name'); //поле ввода имени
-let statusInput = popup.querySelector('.popup__input_set_status'); //поле ввода статуса
 
 //Функция отправки данных формы
 function formSubmitHandler (evt) {
   evt.preventDefault();//отменяем перезагрузку страницы
 
-  let pasteName = document.querySelector('.profile__info-title');//сюда вставляем имя (заголовок на странице)
-  let pasteStatus = document.querySelector('.profile__info-status');// сюда вставляем статус (подзаголовок на странице)
+
   let name //объявили переменную, в которой хранится введенное значение имени
   let status // объявили переменную, в которой хранится введенное значение статуса
 
-  //здесь сброс значения имени, если введено пустое поле
-  if (nameInput.value !== "") {
-    name = nameInput.value; //берем значение из поля ввода
-  } else {
-    name = pasteName.textContent; //значение на странице оставляем прежним
-    nameInput.value = name; // а плейсхолдер берем уже со страницы
-  }
 
-  //здесь сброс значения статуса, если введено пустое поле
-  if (statusInput.value !== "") {
-    status = statusInput.value; //берем значение из поля ввода
-  } else {
-    status = pasteStatus.textContent; //значение на странице оставляем прежним
-    statusInput.value = status; // а плейсхолдер берем уже со страницы
-  }
+  name = nameInput.value; //берем значение из поля ввода
+  status = statusInput.value; //берем значение из поля ввода
+
 
   pasteName.textContent = name;//переносим значение инпута в заголовок
   pasteStatus.textContent = status; //переносим значение статуса на страницу
 
   popupClose();//Закрываем popup
 }
+
+//вызываем функцию popupOpen при клике
+popupOpenButton.addEventListener('click', popupOpen);
+//вызываем функцию удаления popup_opened, если нажали на крестик
+popupCloseButton.addEventListener('click', popupClose);
 
 popup.addEventListener('submit', formSubmitHandler); //выполняем функцию отправки формы
 
