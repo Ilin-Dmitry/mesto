1b1f4cd21c4eafdb35204b9e1002c89d793c4efb
Dmitry Ilin <dm-ilin@mail.ru>
2022-04-14 10:11:09 +0400

    bug/1rev в PopupWithImage вынесены переменные в конструктор

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,12 +1,16 @@
 import { Popup } from './Popup.js'
 
 export class PopupWithImage extends Popup {
-  open = (name, link) => {
+  constructor (popupSelector) {
+    super (popupSelector);
     this._image = this._popup.querySelector('.popup__image');
     this._title = this._popup.querySelector('.popup__image-title');
-  this._image.src = link;
-  this._image.alt = name;
-  this._title.textContent = name;
-  super.open();
+  }
+
+  open = (name, link) => {
+    this._image.src = link;
+    this._image.alt = name;
+    this._title.textContent = name;
+    super.open();
   };
 }
